622313a1c61a4a2c5d4b94ba000be93a0d37989f
ahiru3test <ahiru3test@gmail.com>
2019-11-19 06:36:59 +0000

    19/11/19 15:36

---
+++ main/aMain.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+//GMain is struct
+type GMain struct {
+	i int
+	n int
+	s string
+}
+
+//NewGMain is GMain's constructor
+func NewGMain (i int, n int) *GMain {
+    p := new(GMain)
+    p.i = i
+    p.n = n
+    return p
+}
+
+func main() {
+	gm := NewGMain(111, 222)
+    fmt.Printf("i: %d n: %d \n", gm.i, gm.n)
+}
