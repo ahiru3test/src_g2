9d2a69c08fa4b11f8eeb3647b514c889cf4a4b3e
ahiru3test <ahiru3test@gmail.com>
2020-01-28 04:35:48 +0000

    20/01/28 13:35

--- lib/aaa.go
+++ lib/aaa.go
@@ -1,11 +1,7 @@
 package main
 
-//Import
 import (
-    "encoding/csv"
-    "fmt"
-	"os"
-	"lib"
+	"fmt"
 )
 
 //Taiyaki Class
@@ -20,24 +16,4 @@ func (t Taiyaki) Atama() {
 func (t Taiyaki) Shippo() {
     fmt.Println("たい焼きの尻尾にはあんこがほとんど入っていない")
     fmt.Println("しかしカリカリしていて美味しい")
-}
-
-//Main
-func main() {
-    file, err := os.Open("./aaa.csv")
-    if err != nil {
-        panic(err)
-    }
-    defer file.Close()
-
-    reader := csv.NewReader(file)
-    var line []string
-
-    for {
-        line, err = reader.Read()
-        if err != nil {
-            break
-        }
-        fmt.Println(line)
-    }
 }
