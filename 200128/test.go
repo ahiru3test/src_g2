fa98eb70a24a5d9874711862f78c2484b147b965
ahiru3test <ahiru3test@gmail.com>
2020-01-28 04:20:01 +0000

    20/01/28 13:20

---
+++ 200128/test.go
@@ -0,0 +1,42 @@
+package main
+
+//Import
+import (
+    "encoding/csv"
+    "fmt"
+    "os"
+)
+
+//Taiyaki Class
+type Taiyaki struct{}
+
+//Atama is Taiyaki's Atama
+func (t Taiyaki) Atama() {
+    fmt.Println("たい焼きの頭の方にはあんこがいっぱい入っている")
+}
+
+//Shippo is Taiyaki's Shippo
+func (t Taiyaki) Shippo() {
+    fmt.Println("たい焼きの尻尾にはあんこがほとんど入っていない")
+    fmt.Println("しかしカリカリしていて美味しい")
+}
+
+//Main
+func main() {
+    file, err := os.Open("./aaa.csv")
+    if err != nil {
+        panic(err)
+    }
+    defer file.Close()
+
+    reader := csv.NewReader(file)
+    var line []string
+
+    for {
+        line, err = reader.Read()
+        if err != nil {
+            break
+        }
+        fmt.Println(line)
+    }
+}
